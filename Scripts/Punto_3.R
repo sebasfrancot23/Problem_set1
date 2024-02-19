0da49ac4bd3d89a3c2e767d4cf497608fbcdbb9d
Sebastian Franco <s.francot1@uniandes.edu.co>
2024-02-18 23:01:24 -0500

    Adiciones

--- Scripts/Punto_3.R
+++ Scripts/Punto_3.R
@@ -93,12 +93,17 @@ stargazer(Errores, type = "text", title = "Medidas de ajuste",
 DB_predict = data.frame(age = DB_aux$age,
                     Ingresos_porhora = DB_aux$Ingresos_porhora,
                     Predicciones = predicciones)
+
+#El valor de edad que máxima el salario.
+Age_max = unique(DB_predict[DB_predict$Predicciones == max(DB_predict$Predicciones), "age"])
+
 #El gráfico
 png(filename = paste0(path, "Views/Scatter_age_ingresos_prediccion.png"),
     width = 1464, height = 750)
 ggplot(DB_predict, aes(x = age, y = Ingresos_porhora/1000)) +
   geom_point(color = "blue") +  
   geom_line(aes(y = predicciones/1000), size = 2) + 
+  geom_vline(xintercept = Age_max, linetype = "dashed", color = "red") +
   labs(y = "Ingreso por hora", x = "Edad", caption = "Cifras en miles de pesos.") +
   scale_y_continuous(n.breaks = 6) +
   theme(plot.title = element_text(hjust = 0.5, size = 15),
@@ -112,7 +117,38 @@ ggplot(DB_predict, aes(x = age, y = Ingresos_porhora/1000)) +
         axis.line = element_line(color = "black", size = 1))
 dev.off()                  
 
+# Efecto edad sobre ingresos ----------------------------------------------
+#Se define una función auxiliar del efecto marginal de la edad sobre los ingresos.
+MG_edad = function(Data, lm_object){
+  Efecto =  lm_object[["coefficients"]][2] + 2*lm_object[["coefficients"]][3]*Data
+  return(Efecto)
+}
 
+#Se evalúa la función: 
+DB_aux$Efecto_marginal = MG_edad(DB_aux$age, lm_age)
+
+#El gráfico:
+png(filename = paste0(path, "Views/Efecto_edad_ingresos.png"),
+    width = 1464, height = 750)
+ggplot(DB_aux, aes(x = age, y = Efecto_marginal)) +
+  geom_line(size = 2, color = "skyblue", fill = "black") + 
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+  geom_vline(xintercept = Age_max+0.6, linetype = "dashed", color = "red") +
+  geom_text(aes(label = ifelse(age == Age_max, paste0(as.character(Age_max), " años")
+                               , "")), vjust = -0.3, hjust = -0.2, color = "black") +
+  labs(title = "Efecto marginal de la edad sobre Ingresos_porhora" ,
+       y = "Efecto marginal", x = "Edad") +
+  scale_y_continuous(n.breaks = 6, expand = c(0,0)) +
+  scale_x_continuous(n.breaks = 6, expand = c(0,0)) +
+  theme(plot.title = element_text(hjust = 0.5, size = 15),
+        plot.caption = element_text(size = 15),
+        axis.title.x = element_text(size = 14),
+        axis.title.y = element_text(size = 14),
+        axis.text.x = element_text(size = 10),
+        axis.text.y = element_text(size = 10),
+        panel.grid.major = element_blank(),  
+        panel.grid.minor = element_blank(), 
+        axis.line = element_line(color = "black", size = 1))
 
 
 
